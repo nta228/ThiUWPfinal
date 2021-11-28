d98c5d532cc24eea97bcbe62c028985ff7dc5a8c
nta228 <64982003+nta228@users.noreply.github.com>
2021-11-28 12:51:01 +0700

    Add files via upload

---
+++ Models/ContactModel.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThiUWP.Entities;
+
+namespace ThiUWP.Models
+{
+    public class ContactModel
+    {
+        private static string _selectStatementWithConditionTemplate = "SELECT * FROM contacts WHERE PhoneNumber like @keyword";
+        public ContactModel()
+        {
+            DatabaseMigration.UpdateDatabase();
+        }
+        public bool Save(Contact contact)
+        {
+            try
+            {
+                using (SqliteConnection cnn = new SqliteConnection($"Filename={DatabaseMigration._databasePath}"))
+                {
+                    cnn.Open();
+                    SqliteCommand command = new SqliteCommand("INSERT INTO contacts (PhoneNumber, Name)" +
+                " values (@phoneNumber, @name)", cnn);
+                    command.Parameters.AddWithValue("@phoneNumber", contact.PhoneNumber);
+                    command.Parameters.AddWithValue("@name", contact.Name);
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public List<Contact> FindAll()
+        {
+            List<Contact> result = new List<Contact>();
+            try
+            {
+                using (SqliteConnection cnn = new SqliteConnection($"Filename={DatabaseMigration._databasePath}"))
+                {
+                    cnn.Open();
+                    SqliteCommand command = new SqliteCommand("SELECT * FROM contacts", cnn);
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var phoneNumber = reader.GetString(0);
+                        var name = reader.GetString(1);
+                        var contact = new Contact()
+                        {
+                            PhoneNumber = phoneNumber,
+                            Name = name
+                        };
+                        result.Add(contact);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+        public List<Contact> SearchByKeyword(string keyword)
+        {
+            List<Contact> contacts = new List<Contact>();
+            try
+            {
+                using (SqliteConnection cnn = new SqliteConnection($"Filename={DatabaseMigration._databasePath}"))
+                {
+                    cnn.Open();
+                    SqliteCommand cmd = new SqliteCommand(_selectStatementWithConditionTemplate, cnn);
+                    cmd.Parameters.AddWithValue("@keyword", "%" + keyword + "%");
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var phoneNumber = Convert.ToString(reader["PhoneNumber"]);
+                        var name = Convert.ToString(reader["Name"]);
+                        var contact = new Contact()
+                        {
+                            PhoneNumber = phoneNumber,
+                            Name = name
+                        };
+
+                        contacts.Add(contact);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return contacts;
+        }
+    }
+}
--- Models/DatabaseMigration.cs
+++ Models/DatabaseMigration.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace ThiUWP.Models
 {
-    puclic class DatabaseMigration
+    public class DatabaseMigration
     {
-        internal static void UpdateDabase()
+        private static string _databaseFile = "mycontact.db";
+        public static string _databasePath;
+        private static string _createContactTable = "CREATE TABLE IF NOT EXISTS contacts " +
+            "(PhoneNumber NVARCHAR(100) PRIMARY KEY," +
+            "Name NVARCHAR(255) NOT NULL)";
+        public async static void UpdateDatabase()
         {
-            throw new NotImplementedException();
+            //Tạo đường dẫn.
+            await ApplicationData.Current.LocalFolder.CreateFileAsync(_databaseFile, CreationCollisionOption.OpenIfExists);
+            _databasePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, _databaseFile);
+            using (SqliteConnection cnn = new SqliteConnection($"Filename = {_databasePath}"))
+            {
+                cnn.Open();
+                SqliteCommand command = new SqliteCommand(_createContactTable, cnn);
+                command.ExecuteNonQuery();
+
+            }
         }
     }
 }
