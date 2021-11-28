dc4c2c5edca794c41ac17d06380bfc48338be4c3
nta228 <64982003+nta228@users.noreply.github.com>
2021-11-28 12:50:26 +0700

    Add files via upload

---
+++ Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThiUWP.Entities
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string NumberPhone { get; internal set; }
+    }
+}
